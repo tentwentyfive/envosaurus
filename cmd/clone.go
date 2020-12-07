8e3b082cbbb6de1bb874d5127d468474f356e1a0
Dan Swain <dan.t.swain@gmail.com>
2020-12-06 19:05:53 -0500

    Review feedback

--- cmd/clone.go
+++ cmd/clone.go
@@ -76,14 +76,4 @@ func init() {
 
 	rootCmd.PersistentFlags().StringVarP(&repoConfig, "repo-config", "r", defaultRepoConfig, "Path to repo config json file")
 	rootCmd.MarkFlagRequired("repo-config")
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// cloneCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// cloneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
